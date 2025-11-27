c32df87c0589be82cd1e635e34ac8f06a113cd5c
Aryan Singh <aryanskk216@gmail.com>
2025-11-27 19:26:47 +0530

    Initial Commit

---
+++ HousePlants/HousePlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousePlantsApp.swift
+//  HousePlants
+//
+//  Created by Aryan Signh on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HousePlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HousePlantsTests/HousePlantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HousePlantsTests.swift
+//  HousePlantsTests
+//
+//  Created by Aryan Signh on 27/11/25.
+//
+
+import Testing
+@testable import HousePlants
+
+struct HousePlantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
